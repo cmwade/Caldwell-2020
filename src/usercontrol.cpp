b8e4d9fb6b3dc9742adc691added4725c90ef875
2775Josh <65926085+2775Josh@users.noreply.github.com>
2020-06-17 19:12:05 -0500

    Updated usercontrol.cpp

--- src/usercontrol.cpp
+++ src/usercontrol.cpp
@@ -1,7 +1,20 @@
 #include "vex.h"
+#include "odom.h"
 
 void usercontrol(void) {
-  while (1) {
-    wait(20, msec);
+  float throttle;
+  float strafe;
+  float turn;
+  positiontrackingtask = task( positionTrack );
+  while(1) {
+        throttle = con.Axis3.value();
+        strafe = con.Axis4.value();
+        turn = con.Axis1.value();
+
+        LF.spin(directionType::fwd, (throttle+strafe+turn), velocityUnits::pct);
+        LB.spin(directionType::fwd, (throttle-strafe+turn), velocityUnits::pct);
+        RB.spin(directionType::fwd, (throttle+strafe-turn), velocityUnits::pct);
+        RF.spin(directionType::fwd, (throttle-strafe-turn), velocityUnits::pct);
+        wait(20, msec);
   }
 }
