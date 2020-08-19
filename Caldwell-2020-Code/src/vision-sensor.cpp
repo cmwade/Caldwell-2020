a9eb44d7c128e81bd58392181ce5bda361d689ca
Charlie Wade <cmwade731@gmail.com>
2020-08-19 14:47:59 -0500

    configured vision sensor

--- Caldwell-2020-Code/src/vision-sensor.cpp
+++ Caldwell-2020-Code/src/vision-sensor.cpp
@@ -1,11 +1,11 @@
 /*vex-vision-config:begin*/
 #include "vex.h"
-vex::vision::signature SIG_1 = vex::vision::signature (1, 0, 0, 0, 0, 0, 0, 2.5, 0);
-vex::vision::signature SIG_2 = vex::vision::signature (2, 0, 0, 0, 0, 0, 0, 2.5, 0);
+vex::vision::signature BLUE_BALL = vex::vision::signature (1, -3457, -2201, -2829, 7663, 11119, 9391, 3, 0);
+vex::vision::signature RED_BALL = vex::vision::signature (2, 5267, 7735, 6501, -943, 1, -471, 3, 0);
 vex::vision::signature SIG_3 = vex::vision::signature (3, 0, 0, 0, 0, 0, 0, 2.5, 0);
 vex::vision::signature SIG_4 = vex::vision::signature (4, 0, 0, 0, 0, 0, 0, 2.5, 0);
 vex::vision::signature SIG_5 = vex::vision::signature (5, 0, 0, 0, 0, 0, 0, 2.5, 0);
 vex::vision::signature SIG_6 = vex::vision::signature (6, 0, 0, 0, 0, 0, 0, 2.5, 0);
 vex::vision::signature SIG_7 = vex::vision::signature (7, 0, 0, 0, 0, 0, 0, 2.5, 0);
-vex::vision vs = vex::vision (vex::PORT1, 50, SIG_1, SIG_2, SIG_3, SIG_4, SIG_5, SIG_6, SIG_7);
+vex::vision vs = vex::vision (vex::PORT1, 50, BLUE_BALL, RED_BALL, SIG_3, SIG_4, SIG_5, SIG_6, SIG_7);
 /*vex-vision-config:end*/
