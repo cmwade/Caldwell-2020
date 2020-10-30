e191ba51d7a102f8307378d39890ca5a76845d49
Josh <joshua.bernheisel@gmail.com>
2020-10-29 20:37:37 -0500

    Commented on miscfunctions

--- Caldwell-2020-Code/src/miscFunctions.cpp
+++ Caldwell-2020-Code/src/miscFunctions.cpp
@@ -1,13 +1,13 @@
 #include "vex.h"
 
-void openIntakes(bool waitForCompletion) {
+void openIntakes(bool waitForCompletion) { //Sinple open intakes function.
   IntakeL.resetRotation();
   IntakeR.resetRotation();
   IntakeL.startRotateTo(-240,rotationUnits::deg,100,velocityUnits::pct);
   IntakeR.rotateTo(-240,rotationUnits::deg,100,velocityUnits::pct,waitForCompletion);
 }
 
-void openIntakesWide() {
+void openIntakesWide() { //More complex open intakes function which prevents axle twisting and motor burnout.
   IntakeL.spin(directionType::rev,12,voltageUnits::volt);
   IntakeR.spin(directionType::rev,12,voltageUnits::volt);
   float gains[5] = {100,100,100,100,100};
@@ -18,7 +18,7 @@ void openIntakesWide() {
   float lStart;
   float rStart;
   Brain.Screen.clearScreen();
-  while (avgGain > 1) {
+  while (avgGain > 1) { //Continues to spin while the intakes are moving.
     lStart = IntakeL.rotation(rotationUnits::deg);
     rStart = IntakeR.rotation(rotationUnits::deg);
     task::sleep(25);
@@ -29,14 +29,8 @@ void openIntakesWide() {
       gains[n] = gains[n+1];
     }
     gains[4] = totalGain;
-    avgGain = (gains[0]+gains[1]+gains[2]+gains[3]+gains[4])/5;
+    avgGain = (gains[0]+gains[1]+gains[2]+gains[3]+gains[4])/5; //Calculates the average gain over the last 5 time intervals.
     task::sleep(15);
-    /*Brain.Screen.printAt(1, 20, "%f", gains[0]);
-    Brain.Screen.printAt(1, 40, "%f", gains[1]);
-    Brain.Screen.printAt(1, 60, "%f", gains[2]);
-    Brain.Screen.printAt(1, 80, "%f", gains[3]);
-    Brain.Screen.printAt(1, 100, "%f", gains[4]);
-    Brain.Screen.printAt(1,140,"%f",avgGain);*/
   }
   IntakeL.stop(hold);
   IntakeR.stop(hold);
@@ -52,7 +46,7 @@ void stopIntakes() {
   IntakeR.stop(coast);
 }
 
-void unfold() {
+void unfold() { //Used to unfold the robot precisely.
   RollerBack.spin(directionType::fwd, 100, velocityUnits::pct);
   IntakeR.spin(directionType::rev,12,voltageUnits::volt);
   task::sleep(500);
@@ -60,7 +54,7 @@ void unfold() {
   IntakeL.spin(directionType::rev,12,voltageUnits::volt);
 }
 
-void eatBall() {
+void eatBall() { //Closes the actuated intakes around a ball on the field.
   IntakeL.spin(directionType::fwd,100,velocityUnits::pct);
   IntakeR.spin(directionType::fwd,100,velocityUnits::pct);
   task::sleep(500);
@@ -68,14 +62,14 @@ void eatBall() {
   IntakeR.stop();
 }
 
-void descoreBall( int numberOfBalls ) {
+void descoreBall( int numberOfBalls ) { //Descores the specified number of balls from a goal.
   int n = 0;
   float starttime = Brain.timer(timeUnits::msec);
-  while(n < numberOfBalls) {
+  while(n < numberOfBalls) { //Repeats until a certain number of balls have been descored.
     IntakeL.stop();
     IntakeR.stop();
     RollerMain.stop();
-    starttime = Brain.timer(timeUnits::msec);
+    starttime = Brain.timer(timeUnits::msec); //Doesn't continue until the ball has both pressed and released the ball detector.
     while(BallDetector.pressing()){
       IntakeL.spin(directionType::fwd,100,velocityUnits::pct);
       IntakeR.spin(directionType::fwd,100,velocityUnits::pct);
@@ -89,7 +83,7 @@ void descoreBall( int numberOfBalls ) {
     IntakeL.stop();
     IntakeR.stop();
     RollerMain.stop();
-    if (Brain.timer(timeUnits::msec) - starttime > 250) {
+    if (Brain.timer(timeUnits::msec) - starttime > 250) { //Prevents oscillation from counting as a false descore.
     n+=1;
     }
   }
